a9cd0043bfd79410fe91414eaddc388ee53a585a
U-WILLIAM-ASUS\William <William@william-asus.(none)>
2008-04-23 00:27:42 +0200

    Removed white space.

--- ruby_email.rb
+++ ruby_email.rb
@@ -1,3 +1,9 @@
-module RubyEmail
-  
+module RubyEmail  
 end
+
+require 'lib/base'
+require 'lib/header_field'  
+require 'lib/body'
+require 'lib/body_part'
+require 'lib/message'
+require 'lib/message_parser'
