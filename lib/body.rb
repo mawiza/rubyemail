e4fff638d9cc47dfae40dad4cf745d2a32de7232
U-WILLIAM-ASUS\William <William@william-asus.(none)>
2008-04-23 00:25:01 +0200

    Fixed the attr_accessor bug, allready implemented the accessor using defs.

--- lib/body.rb
+++ lib/body.rb
@@ -1,15 +1,11 @@
 module RubyEmail
   class Body < Base      
     attr_accessor :content
-    attr_accesor :body_parts
+    attr_reader :body_parts
     
     def body_parts=(value)
       @multipart = true
       @body_parts = value
-    end
-    
-    def body_parts
-      @body_parts
     end
     
     def multipart=(value)
