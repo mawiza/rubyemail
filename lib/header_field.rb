a6882f1d7a3d650496fb96270ad1bab265a47085
U-WILLIAM-ASUS\William <William@william-asus.(none)>
2008-04-23 00:26:19 +0200

    Modified the HeaderField object to validate the name(key) and added a constructor

--- lib/header_field.rb
+++ lib/header_field.rb
@@ -1,5 +1,22 @@
 module RubyEmail
+  
+  # This object is used by the Message and BodyPart objects
   class HeaderField < Base
-    attr_accessor :name, :body
+    
+    attr_reader :name, :body
+        
+    def initialize(name, body)
+      super()           
+      @name, @body = name, body
+      valid_header_field_name?
+    end
+    
+    # Validate the name. Should not be empty or nil
+    def valid_header_field_name?
+      unless (@name and @name.length > 0) and @name
+        raise("Name should not be empty!")
+      end      
+      return true
+    end
   end
 end
