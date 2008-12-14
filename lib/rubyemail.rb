878d811a9d7524c332a85cbba04a75e953500423
William Miles <gmawiza@gmail.com>
2008-12-14 19:31:49 +0100

    Added the Base class to the Rubyemail module.

--- lib/rubyemail.rb
+++ lib/rubyemail.rb
@@ -3,4 +3,15 @@ $:.unshift(File.dirname(__FILE__)) unless
 
 module Rubyemail
   VERSION = '0.0.1'
+  
+  class Base  
+    attr_accessor :raw
+  end
 end
+
+#require 'lib/rubyemail/header_fields'
+#require 'lib/rubyemail/header_field'  
+#require 'lib/rubyemail/body'
+#require 'lib/rubyemail/body_part'
+require 'lib/rubyemail/message'
+#require 'lib/rubyemail/message_parser'
